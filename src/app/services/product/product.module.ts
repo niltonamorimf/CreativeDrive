86239aedfb283aacf1c92efefda2f759d2d332c1
Carlos Ramos <carlosefrfloresta@gmail.com>
2019-01-07 07:15:38 -0200

    Implementarion, REST

--- src/app/services/product/product.module.ts
+++ src/app/services/product/product.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from './product.service';
+import { ApiModule } from '../api/api.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ApiModule
   ],
   providers: [ProductService]
 })
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { PRODUCTS } from './products';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,17 @@ export class ProductService {
 
   private _products;
 
-  constructor() {
+  constructor(private _api: ApiService) {
     this._products = this.newProductsRef(PRODUCTS);
   }
 
   public get products() {
+    this._api.get('getProductList').subscribe( x => console.log(x, 'response: getProductList'));
     return this._products;
   }
 
   public saveProduct(product) {
+    this._api.post('saveProduct', {product: product}).subscribe( x => console.log(x, 'response: saveProduct'));
     return this._products.find( (p, index, arr) => {
        if (p.sku === product.sku) {
         arr[index] = product;
@@ -26,6 +29,7 @@ export class ProductService {
   }
 
   public saveQuotes({sku, quotes}) {
+    this._api.post('saveProducQuotes', {quotes: quotes, sku: sku}).subscribe( x => console.log(x, 'response: saveProducQuotes'));
     return this._products.find( (p, index, arr) => {
       if (p.sku === sku) {
        arr[index].quotes = quotes;
@@ -35,10 +39,13 @@ export class ProductService {
   }
 
   public chargeProducts(skuList) {
+    this._api.post('chargeProducts', {skuList: skuList}).subscribe( x => console.log(x, 'response: chargeProducts'));
     return skuList;
   }
 
   public getProduct(sku) {
+
+    this._api.get('getProduct', {sku: sku}).subscribe( x => console.log(x, 'response: getProduct'));
     return this.newProductsRef(this._products.find( p => p.sku === sku));
   }
 
